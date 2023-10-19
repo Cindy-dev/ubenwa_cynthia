20b330d2d5d7ec137e13e600d092a0eda956f7b7
Cindy-dev <ikwuegbucynthia2000@gmail.com>
2023-10-19 15:05:01 +0100

    file import

---
+++ lib/presentation/utils/home_strings.dart
@@ -0,0 +1,8 @@
+class HomeStrings {
+  static String breakdown = "assets/images/breakdown.png";
+  static String challenge = "assets/images/challenge.png";
+  static String chart = "assets/images/chart.png";
+  static String predictCry = "assets/images/predict_cry.png";
+  static String sun = "assets/images/sun-fog.png";
+  static String wifi = "assets/images/wifi-square.png";
+}
